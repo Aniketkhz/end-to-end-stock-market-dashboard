a669c1be36d8a19eb54f5fdbda1de436018cdf36
Aniketkhz <aniketkhadanga2@gmail.com>
2025-09-28 17:46:36 +0530

    filter data

--- dashboard/app.py
+++ dashboard/app.py
@@ -19,7 +19,7 @@ end_date = st.sidebar.date_input('End Date', df['Date'].max())
 # Filter data based on user input
 filtered_data = df[(df['Ticker'] == ticker) & (df['Date'] >= str(start_date)) & (df['Date'] <= str(end_date))]
 
-# Line chart for Close price over time
+# Line chart for Close price over 
 st.subheader('Close Price Over Time')
 fig_close = px.line(filtered_data, x='Date', y='Close', title='Close Price')
 st.plotly_chart(fig_close)
